962d578310e35ace9f4b0154523d7cfdbb12400e
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-02-13 23:46:31 +0000

    Update Exercise003.scala
    committing part of excercise003.

--- src/main/scala/com/techreturners/exercise003/Exercise003.scala
+++ src/main/scala/com/techreturners/exercise003/Exercise003.scala
@@ -1,13 +1,26 @@
-package com.techreturners.exercise003
+ackage com.techreturners.exercise003
+
 
 class Exercise003 {
 
   def getIceCreamCode(iceCreamFlavour: String): Int = {
-    throw new UnsupportedOperationException("You can delete this line and add your code here.")
+
+    return iceCreamFlavour match {
+      case "Pistachio" => 0
+      case "Raspberry Ripple" => 1
+      case "Vanilla" => 2
+      case "Mint Chocolate Chip" => 3
+      case "Chocolate" => 4
+      case "Mango Sorbet" => 5
+      case _ => 6
+    }
   }
+
 
   def iceCreamFlavours: Array[String] = {
     throw new UnsupportedOperationException("You can delete this line and add your code here.")
+ 
+
   }
 
 }
