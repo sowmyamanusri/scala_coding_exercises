853863e3422156c3f846c32a0147a57ae97c2605
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-02-13 20:30:45 +0000

    Update Exercise001.scala
    Completed the implementation of exercise001

--- src/main/scala/com/techreturners/exercise001/Exercise001.scala
+++ src/main/scala/com/techreturners/exercise001/Exercise001.scala
@@ -7,13 +7,41 @@ class Exercise001 {
   // we don't specify it in the code
   // https://docs.scala-lang.org/tour/basics.html
 
-  def capitalizeWord(word: String): String = ""
+  def capitalizeWord(word: String): String = {
 
-  def generateInitials(firstName: String, lastName: String) = ""
+   word.toLowerCase.capitalize
 
-  def addVat(initialPrice: Double, interestRate: Double) : Double = 0.0
+  }
 
-  def reverse(sentence: String) : String = ""
+  def generateInitials(firstName: String, lastName: String) = {
+
+    firstName.charAt(0).toString + "." + lastName.charAt(0).toString
+
+  }
+
+  def addVat(initialPrice: Double, interestRate: Double) : Double = {
+
+    if (interestRate == 0) return initialPrice
+
+    var percentValue: Double = (initialPrice * interestRate) / 100
+
+    if ((percentValue - Math.floor(percentValue)) != 0) {
+
+      val totalPrice: Double = initialPrice + percentValue
+      return "%.2f".format(totalPrice).toDouble
+    }
+    else {
+      val totalPrice: Double = initialPrice + percentValue
+      return totalPrice
+    }
+
+  }
+
+  def reverse(sentence: String) : String = {
+
+     sentence.reverse
+
+  }
 
   /**
    * Here we show using a Scala sequence which is a type of collection
@@ -23,8 +51,17 @@ class Exercise001 {
    * @return
    */
   def countLinuxUsers(users: Seq[User]): Int = {
-    // Add your code here
-    0
+
+   var count:Int =0
+    if((users == null) || (users.length == 0)) return 0
+
+    users.foreach(user => {
+      if (user == null) return  0
+      if (user.osType == "Linux") {
+        count = count + 1
+      }
+    })
+    return  count;
   }
 
 }
