cea72e59194a151b34a7ac2c6859fd01c65c75e5
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-02-13 23:43:44 +0000

    Update Exercise002.scala
    committing exercise 002

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,8 +2,17 @@ package com.techreturners.exercise002
 
 class Exercise002 {
 
-  def isFromManchester(/*person: Person*/): Boolean = false
+  def isFromManchester(person: Person): Boolean = {
+    if (person.city == "Manchester" ) return true
+    else
+      return false
+  }
 
-  def canWatchFilm(/*person: Person, ageLimit: Int */): Boolean = false
+  def canWatchFilm(person: Person, ageLimit: Int): Boolean = {
+
+    if (person.age > 18) return true
+    else
+      return false
+  }
 
 }
