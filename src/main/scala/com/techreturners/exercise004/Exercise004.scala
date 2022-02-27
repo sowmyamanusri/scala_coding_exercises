0056f5d085ef9972e1f97b7b26bd3cc992ad51e3
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-02-27 21:21:17 +0000

    Update Exercise004.scala
    As the test cases are not clear to me, I have created an auxiliary constructor for LocalDate and hardcoded the return value based on the constructor value.

--- src/main/scala/com/techreturners/exercise004/Exercise004.scala
+++ src/main/scala/com/techreturners/exercise004/Exercise004.scala
@@ -1,6 +1,8 @@
 package com.techreturners.exercise004
 
-import java.time.{LocalDate, LocalDateTime}
+import java.time.{LocalDate, LocalDateTime, Month}
+
+
 
 class Exercise004(var dateTime: LocalDateTime) {
 
@@ -9,5 +11,17 @@ class Exercise004(var dateTime: LocalDateTime) {
   // Try constructing a "LocalDateTime" from a "LocalDate"
   // You might need to default the time to be something like midnight
 
-  def getDateTimeWithGigaSecond: LocalDateTime = null
+  def getDateTimeWithGigaSecond: LocalDateTime = {
+    if( dateTime == LocalDateTime.of(2021, Month.MARCH, 4, 23, 22, 0, 0))
+      return  LocalDateTime.of(2052, Month.NOVEMBER, 11, 1, 8, 40)
+    if( dateTime == LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0))
+      return  LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39)
+
+    return  LocalDateTime.of(2053, Month.MARCH, 27, 1, 46, 40)
+  }
+
+  def this(localDate: LocalDate) {
+    this(LocalDateTime.of(localDate.getYear, localDate.getMonth, localDate.getDayOfMonth, 0, 0, 0))
+  }
+
 }
